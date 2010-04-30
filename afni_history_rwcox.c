2e908ee7975b59e5e8a05263c4c1e4b58896b04c
NeuroDebian <neurodebian@expert.pymvpa.org>
2010-04-30 05:17:21 -0400

    Importing AFNI 0.20100429~dfsg.1 Imported from tarball: AFNI_latest.tgz Original MD5: 42ac0d8fe305e53f43128906294c6c89 AFNI claimed to be version: 0.20091231.1431

--- afni_history_rwcox.c
+++ afni_history_rwcox.c
@@ -44,6 +44,18 @@
 afni_history_struct rwcox_history[] = {
 /*=====BELOW THIS LINE=====*/
 
+ { 29 , APR , 2010 , RWC , "3dTcorrelate" , MICRO , TYPE_NEW_OPT ,
+   "add -ktaub option" ,
+   NULL } ,
+
+ { 29 , APR , 2010 , RWC , "InstaCorr" , MICRO , TYPE_NEW_OPT ,
+   "Add Kendall Tau_b to the correlation menu" ,
+   "For fun, and as a start towards something else." } ,
+
+ { 28 , APR , 2010 , RWC , "afni" , MINOR , TYPE_MODIFY ,
+   "Hide markers stuff from the user" ,
+   "Unless AFNI_ENABLE_MARKERS is set to YES" } ,
+
  { 8 , APR , 2010 , RWC , "3dBandpass" , MINOR , TYPE_BUG_FIX ,
    "Fix -localPV implementation" ,
    NULL } ,
