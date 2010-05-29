8d1a4b6845eb92a5e821901de89f8a792cbe9f41
NeuroDebian <neurodebian@expert.pymvpa.org>
2010-05-29 05:17:23 -0400

    Importing AFNI 0.20100528~dfsg.1 Imported from tarball: AFNI_latest.tgz Original MD5: 294f83f960e35d5994e3ff7fbf8aeb43 AFNI claimed to be version: 0.20091231.1431

--- afni_pplug_instacorr.c
+++ afni_pplug_instacorr.c
@@ -177,7 +177,7 @@ PLUGIN_interface * ICOR_init( char *lab )
                                 PLUGIN_CALL_VIA_MENU ,
                                 (char *(*)())ICOR_main  ) ;
 
-   sprintf(sk,"%sSetup+Keep",lab) ; sprintf(sc,"%sSetup+Close",lab) ;
+   sprintf(sk,"%sSetup+Keep",lab) ; sprintf(sc,"%sSetup+Quit",lab) ;
    PLUTO_set_runlabels( plint , sk , sc ) ;
 
    /*--------- make interface lines -----------*/
@@ -1171,20 +1171,20 @@ ENTRY("GICOR_init") ;
                                 (char *(*)())GICOR_main  ) ;
    GICOR_plint = plint ;
 
-   PLUTO_set_runlabels( plint , "Setup+Keep" , "Setup+Close" ) ;
+   PLUTO_set_runlabels( plint , "Set+Keep" , "Set+Quit" ) ;
 
    /*--------- make interface lines -----------*/
 
-   PLUTO_add_option ( plint , "Params" , "Params" , TRUE ) ;
-   PLUTO_add_number ( plint , "SeedRad" , 0,16,0,(int)rint(giset->seedrad), TRUE ) ;
+   PLUTO_add_option ( plint , "Parameters" , "Params" , TRUE ) ;
+   PLUTO_add_number ( plint , "SeedRadius" , 0,16,0,(int)rint(giset->seedrad), TRUE ) ;
 
 #ifdef GIC_ALLOW_TTEST
    ntops = (giset->ndset_A == giset->ndset_B) ? 3 : 2 ;
    PLUTO_add_string ( plint , "t-test"  , ntops , topts , giset->ttest_opcode ) ;
 #endif
 
-   PLUTO_add_option ( plint , "Cluster" , "Cluster" , TRUE ) ;
-   PLUTO_add_number ( plint , "Voxels"  , 0,9999,0 , 0,TRUE ) ;
+   PLUTO_add_option ( plint , "Clustering" , "Cluster" , TRUE ) ;
+   PLUTO_add_number ( plint , "Voxels Min"  , 0,9999,0 , 0,TRUE ) ;
 
    if( giset->toplabel != NULL )
      PLUTO_set_toplabel( GICOR_plint , giset->toplabel ) ;
