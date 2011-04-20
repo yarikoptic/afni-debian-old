addaab78b3ea0a770b7c8ed9eb6b9f8bbd5cff25
NeuroDebian <neurodebian@expert.pymvpa.org>
2011-04-20 05:17:18 -0400

    Importing AFNI 0.20110419~dfsg.1 Imported from tarball: AFNI_latest.tgz Original MD5: b2258cd5b0390418e83beef20b633033 AFNI claimed to be version: 0.20101019.1028

--- afni_history_rwcox.c
+++ afni_history_rwcox.c
@@ -43,7 +43,15 @@
 
 afni_history_struct rwcox_history[] = {
 /*=====BELOW THIS LINE=====*/
-  { 18 , APR , 2011 , RWC , "afni_graph" , MICRO , TYPE_GENERAL ,
+  { 19 , APR , 2011 , RWC , "afni" , MICRO , TYPE_GENERAL ,
+   "Let user select grid spacing=1 from menu" ,
+   "Helpful for Boxes graphs" } ,
+
+ { 19 , APR , 2011 , RWC , "afni" , MICRO , TYPE_NEW_ENV ,
+   "AFNI_GRAPH_FONT lets user choose font for graph viewer text" ,
+   "9x15bold looks solid on a 100dpi screen, for example." } ,
+
+ { 18 , APR , 2011 , RWC , "afni_graph" , MICRO , TYPE_GENERAL ,
    "Box graphs get sub-brick labels" ,
    "If matrix=1 and user sets AFNI_GRAPH_BOXLAB to 'ATOP', 'MAX', or 'ZERO'.\n"
    "For Shane." } ,
