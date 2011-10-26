abf0cd567a73a5e4c6a189a00d6559e8847187d7
NeuroDebian <neurodebian@expert.pymvpa.org>
2011-10-26 05:17:15 -0400

    Importing AFNI 0.20111025~dfsg.1 Imported from tarball: AFNI_latest.tgz Original MD5: 9dd0c30221b2e70b84a5d5a51893b2bc AFNI claimed to be version: 0.20110526.1457

--- afni_history_rickr.c
+++ afni_history_rickr.c
@@ -49,6 +49,18 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 25, OCT, 2011, RCR, "timing_tool.py", MINOR, TYPE_ENHANCE,
+   "process married files with current operations",
+   "1. AfniMarriedTiming inherits from AfniData (instead of local copies)\n"
+   "2. add all AfniTiming methods to AfniMarriedTiming (as married timing)\n"
+   "3. rename AfniMarriedTiming back to AfniTiming (but now married)"
+ } ,
+
+ { 25, OCT, 2011, RCR, "gen_ss_review_scripts.py", MINOR, TYPE_ENHANCE,
+   "look for more file name variants, including with '_' separators",
+   "Added for J Weisberg."
+ } ,
+
  { 20, OCT, 2011, RCR, "afni_general", MINOR, TYPE_MODIFY,
    "changed most resampling programs to deoblique inputs upon read",
    "THD_open*_dataset() was followed by THD_make_cardinal().\n"
