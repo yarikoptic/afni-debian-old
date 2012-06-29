e6ecbb35d973a86919c64dbafa7c2f67fabc9171
NeuroDebian <neurodebian@lego.pymvpa.org>
2012-06-29 05:17:10 -0400

    Importing AFNI 0.20120628~dfsg.1 Imported from tarball: AFNI_latest.tgz Original MD5: d26a9d83570e5532c196a07c2e3d3789 AFNI claimed to be version: 0.20111221.1014

--- afni_history_rickr.c
+++ afni_history_rickr.c
@@ -49,6 +49,21 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 28, JUN, 2012, RCR, "@Install_Cambridge_Demo", MINOR, TYPE_NEW_PROG,
+   "program is for installing Cambridge resting state InstaCorr demo",
+   NULL
+ } ,
+
+ { 28, JUN, 2012, RCR, "afni_proc.py", MICRO, TYPE_BUG_FIX,
+   "fixed help error regarding IM",
+   "Thanks to I Blair for noting it."
+ } ,
+
+ { 27, JUN, 2012, RCR, "3dTstat", MICRO, TYPE_NEW_OPT,
+   "added -nzmedian, requested on message board",
+   NULL
+ } ,
+
  { 25, JUN, 2012, RCR, "gen_group_command.py", MICRO, TYPE_ENHANCE,
    "added help for -factors and 3dANOVA3 -type 4 examples",
    NULL
