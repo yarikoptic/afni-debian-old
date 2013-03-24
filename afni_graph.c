aa4eb92a1b6567529aebf1f23e7f20d72698e25c
NeuroDebian <neurodebian@lego.pymvpa.org>
2013-03-24 05:17:10 -0400

    Importing AFNI 0.20130323~dfsg.1 Imported from tarball: AFNI_latest.tgz Original MD5: 7dac8615d846ae298bb3cb86cf98668a AFNI claimed to be version: 0.20111221.1014

--- afni_graph.c
+++ afni_graph.c
@@ -221,6 +221,7 @@ ENTRY("new_MCW_grapher") ;
                        "1      = move to 1st time point\n"
                        "l      = move to last time point\n"
                        "L      = turn AFNI logo on/off\n"
+                       "F      = turn threshold 'Fading' on/off\n"
                        "v/V    = Video up/down in time\n"
                        "r/R    = Video ricochet up/down in time\n"
                        "F5     = Meltdown!\n"
@@ -1454,7 +1455,7 @@ ENTRY("GRA_redraw_overlay") ;
       xxx = MAX( grapher->xx_text_2 ,
                  grapher->xorigin[grapher->xc][grapher->yc]-39 ) ;
 
-      if( grapher->init_ignore > 0 ) xxx = MAX( xxx , grapher->xx_text_2p ) ;
+      if( grapher->init_ignore > 0 || grapher->thresh_fade ) xxx = MAX( xxx , grapher->xx_text_2p ) ;
 
       DC_fg_color( grapher->dc , IDEAL_COLOR(grapher) ) ;
       overlay_txt( grapher, xxx , GB_DLY-15 , strp ) ;
@@ -1571,6 +1572,10 @@ ENTRY("redraw_graph") ;
 
    if( grapher->init_ignore > 0 ){                    /* 23 May 2005 */
      sprintf(strp,"Ignore%4d",grapher->init_ignore) ;
+     if( grapher->thresh_fade ) sprintf(strp+strlen(strp)," Fade on") ;
+     fd_txt( grapher , xxx , 35, strp) ;
+   } else if( grapher->thresh_fade ){
+     sprintf(strp,"Fade on") ;
      fd_txt( grapher , xxx , 35, strp) ;
    }
 
@@ -1600,7 +1605,8 @@ ENTRY("redraw_graph") ;
        sprintf(strp,"Num%3d:%-3d" , bb,tt ) ;
    }
    fd_line( grapher ,
-            grapher->xx_text_2+rrr+3 , (grapher->init_ignore > 0) ? 41 : 31 ,
+            grapher->xx_text_2+rrr+3 ,
+            (grapher->init_ignore > 0 || grapher->thresh_fade) ? 41 : 31 ,
             grapher->xx_text_2+rrr+3 , 5 ) ;
 
    grapher->xx_text_2p = grapher->xx_text_2+rrr+7 ;  /* 23 May 2005 */
