d19ef4f17d9e692d1befd06be7c94d4a04f095f3
NeuroDebian <neurodebian@lego.pymvpa.org>
2013-05-10 05:17:13 -0400

    Importing AFNI 0.20130509~dfsg.1 Imported from tarball: AFNI_latest.tgz Original MD5: 04665f8f24af0286a5b6cce28beaccf2 AFNI claimed to be version: 0.20111221.1014

--- afni_history_rickr.c
+++ afni_history_rickr.c
@@ -49,6 +49,16 @@
 
 afni_history_struct rickr_history[] = {
 
+ {  9, May, 2013, RCR, "afni_proc.py", MINOR, TYPE_NEW_OPT,
+   "added options -write_3dD_script and -write_3dD_prefix",
+   NULL
+ } ,
+
+ {  8, May, 2013, RCR, "1d_tool.py", MINOR, TYPE_NEW_OPT,
+   "added options -rank, -rank_style, -reverse_rank",
+   NULL
+ } ,
+
  {  6, May, 2013, RCR, "afni_proc.py", MINOR, TYPE_ENHANCE,
    "added -regress_anaticor example; opt implies -mask_segment_anat/_erode",
    NULL
@@ -59,7 +69,7 @@ afni_history_struct rickr_history[] = {
    NULL
  } ,
 
- {  6, May, 2013, RCR, "1d_tool.py", MINOR, TYPE_NEW_OPT,
+ {  6, May, 2013, RCR, "1d_tool.py", MICRO, TYPE_NEW_OPT,
    "added option -transpose_write",
    NULL
  } ,
