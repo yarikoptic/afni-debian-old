90beda0ee46f29b363b8879a06b20cff6b70f8bb
NeuroDebian <neurodebian@lego.pymvpa.org>
2013-12-20 05:17:38 -0500

    Importing AFNI 0.20131219~dfsg.1 Imported from tarball: AFNI_latest.tgz Original MD5: d76cd579507bd6915407aa18d0598227 AFNI claimed to be version: 0.20111221.1014

--- SUMA/SUMA_volume_render.c
+++ SUMA/SUMA_volume_render.c
@@ -2142,6 +2142,11 @@ SUMA_Boolean SUMA_DrawVolumeDO_exp(SUMA_VolumeObject *VO, SUMA_SurfaceViewer *sv
    
    SUMA_ENTRY;
    
+   #ifndef GL_VERSION_2_0
+      /* GL must be old */
+      SUMA_S_Err("Open GL < 2.0, glWindowPos2s() not yet supported (on all machines)");
+      SUMA_RETURN(NOPE);
+   #else
    if (!VO) SUMA_RETURN(NOPE);
    if (!sv) sv = &(SUMAg_SVv[0]);
    
@@ -2525,7 +2530,7 @@ SUMA_Boolean SUMA_DrawVolumeDO_exp(SUMA_VolumeObject *VO, SUMA_SurfaceViewer *sv
    if (gl_bl) glEnable(GL_BLEND);
    else glDisable(GL_BLEND);
    SUMA_RETURN(YUP);
-   
+   #endif
 }
 
 
