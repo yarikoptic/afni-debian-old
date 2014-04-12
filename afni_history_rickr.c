8ff71da3e68072027d386d2f1937462103bb9cfb
NeuroDebian <neurodebian@lego.pymvpa.org>
2014-04-12 05:17:19 -0400

    Importing AFNI 0.20140411~dfsg.1 Imported from tarball: afni_src.tgz Original MD5: 186e51ec2b15b826e491ca4d4a506ce3 AFNI claimed to be version: 0.20111221.1014

--- afni_history_rickr.c
+++ afni_history_rickr.c
@@ -49,6 +49,27 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 10,  Apr, 2014, RCR, "afni-general", MINOR, TYPE_BUG_FIX,
+   "@afni.run.me never made it into Makefile.INCLUDE for distribution",
+   NULL
+ } ,
+
+ { 10,  Apr, 2014, RCR, "1d_tool.py", MINOR, TYPE_NEW_OPT,
+   "added -index_to_run_tr, intended for use by afni_proc.py",
+   NULL
+ } ,
+
+ {  9,  Apr, 2014, RCR, "gen_ss_review_table.py", MAJOR, TYPE_NEW_PROG,
+   "parse output from @ss_review_basic text into spreadsheet format",
+   "This makes it easy to flag outlier subject values.\n"
+   "Thanks to J Jarcho for encouragement."
+ } ,
+
+ {  9,  Apr, 2014, RCR, "gen_ss_review_scripts.py", MICRO, TYPE_MODIFY,
+   "give priority to GCOR files with 'out' in the name",
+   NULL
+ } ,
+
  {  4,  Apr, 2014, RCR, "afni-general", MINOR, TYPE_BUG_FIX,
    "fixed reading NIFTI obliquity w/dglen (lost Mar 22)",
    "Thanks to P Kundu for noting the problem."
