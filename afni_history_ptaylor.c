39d323af49dde5f3595252fc82ac501fe69cb3bc
NeuroDebian <neurodebian@lego.pymvpa.org>
2014-04-30 05:17:17 -0400

    Importing AFNI 0.20140429~dfsg.1 Imported from tarball: afni_src.tgz Original MD5: 88e7b27cfadd8baeb1e8adc06f4849bd AFNI claimed to be version: 0.20111221.1014

--- afni_history_ptaylor.c
+++ afni_history_ptaylor.c
@@ -66,6 +66,16 @@
 afni_history_struct ptaylor_history[] = {
 /*=====BELOW THIS LINE=====*/
 
+{ 29, Apr , 2014 , PT , "3dROIMaker" , MINOR , TYPE_NEW_OPT,
+   "Freedom in neighbor defs; also can keep just N peak values per ROI.",
+   "User can specify face, edge or vertex ngbs. Also, search for N max vals.\n"
+},
+
+{ 29, Apr , 2014 , PT , "3dNetCorr" , MINOR , TYPE_NEW_OPT,
+   "Added new feature: output WB correlations as Zscores.",
+   "Can output WB maps of ROI average time series correlations as Z-scores.\n"
+},
+
 { 21, Apr , 2014 , PT , "1dDW_Grad_o_Mat" , MICRO , TYPE_NEW_OPT,
    "Output grads as rows-- new option switch.",
    "Done at user request.\n"
